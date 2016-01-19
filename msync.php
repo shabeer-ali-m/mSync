afe3ceb4d78ba6aae1922bde3512c3c9d225dd99
shabeer <shabeer@whyable.com>
2016-01-19 16:26:05 +0530

    View sync

--- msync.php
+++ msync.php
@@ -40,6 +40,14 @@ class mSync{
         }
     }
 	
+    private function clean_view($data)
+    {
+        $view_sql=$data['Create View'];
+        $pos=strpos($view_sql,"VIEW ");
+        $data['Create View']='CREATE '.substr($view_sql,$pos);
+        return $data;
+    }
+
     public function create_json()
     {
         $result=$this->conn->query('SHOW FULL TABLES WHERE Table_type !=  "VIEW"');
@@ -58,6 +66,21 @@ class mSync{
             //$struct[$v]=$res->fetch_all(MYSQLI_ASSOC);
 			$struct[$v]=$array;
         }
+
+        //for view
+        $result=$this->conn->query('SHOW FULL TABLES WHERE Table_type =  "VIEW"');
+        $rows = array();
+        while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+                $rows[]=$row[key($row)];
+        }
+        foreach($rows as $v)
+        {
+            $res=$this->conn->query('SHOW CREATE VIEW `'.$v.'`;');
+            $array = array();
+            while($srow = $res->fetch_assoc())
+                $array[] = $this->clean_view($srow);
+            $struct[$v]=$array;
+        }
         
         file_put_contents('schema.json',json_encode($struct,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
 		echo "schema.json updated successfully!";
@@ -73,39 +96,63 @@ class mSync{
     {
         $schema=$this->schema;
         foreach($schema as $table=>$cols){
-            
-            $res=$this->conn->query('DESCRIBE `'.$table.'`');
+            //if view
+            if(isset($cols[0]['View']))
+            {
+                $res=$this->conn->query('SHOW CREATE VIEW `'.$table.'`');
+                $struct = $this->clean_view(mysqli_fetch_array($res,MYSQLI_ASSOC));
+                if($struct['Create View']!=$cols[0]['Create View'])
+                {
+                    $drop_sql="DROP VIEW IF EXISTS $table;";
+                    $this->conn->query($drop_sql);
+                    if(!$this->conn->query($cols[0]['Create View'])){
+                        echo  mysqli_error($this->conn);
+                        echo "\n".$sql;
+                        exit ;
+                    }
+                    else{
+                        echo "View Updated : ".$table."\n";
+                        error_log(PHP_EOL.$drop_sql, 3, "mSync.log");
+                        error_log(PHP_EOL.$cols[0]['Create View'], 3, "mSync.log");
+                    }
 
-            //echo $table.'<br/>';
-			$struct=array();
-			while ($struct[] = mysqli_fetch_array($res,MYSQLI_ASSOC));
-			array_pop($struct);
-			//print_r($struct);
-			//exit(0);
-            /*associating field name*/
-            $struct=$this->assoc_prime($struct,array("key"=>"Field"),true);
-            $cols=$this->assoc_prime($cols,array("key"=>"Field"),true);
-            /*get new field*/
-            $fc=array_diff(array_keys($cols),array_keys($struct));
-            if(count($fc)>0){
-                foreach($fc as $f){
-                    $this->new_field[$table][$f]=$cols[$f];
                 }
             }
-            /*get field to be deleted*/
-            $fd=array_diff(array_keys($struct),array_keys($cols));
-            if(count($fd)>0){
-                foreach($fd as $f){
-                   $this->delete_field[$table][$f]=$struct[$f];
+            else
+            {
+                $res=$this->conn->query('DESCRIBE `'.$table.'`');
+
+                //echo $table.'<br/>';
+                $struct=array();
+                while ($struct[] = mysqli_fetch_array($res,MYSQLI_ASSOC));
+                array_pop($struct);
+                //print_r($struct);
+                //exit(0);
+                /*associating field name*/
+                $struct=$this->assoc_prime($struct,array("key"=>"Field"),true);
+                $cols=$this->assoc_prime($cols,array("key"=>"Field"),true);
+                /*get new field*/
+                $fc=array_diff(array_keys($cols),array_keys($struct));
+                if(count($fc)>0){
+                    foreach($fc as $f){
+                        $this->new_field[$table][$f]=$cols[$f];
+                    }
                 }
-            }
-            /*detect changed fields */
-            foreach($struct as $st=>$s){
-                foreach($cols as $co=>$c){
-                    if($st==$co){
-                        $change=array_merge(array_diff($c,$s),array_diff($s,$c));
-                        if(count($change)>0){
-                            $this->alter[$table][$co]=$c;
+                /*get field to be deleted*/
+                $fd=array_diff(array_keys($struct),array_keys($cols));
+                if(count($fd)>0){
+                    foreach($fd as $f){
+                       $this->delete_field[$table][$f]=$struct[$f];
+                    }
+                }
+                /*detect changed fields */
+                foreach($struct as $st=>$s){
+                    foreach($cols as $co=>$c){
+                        if($st==$co){
+                            $change=array_merge(array_diff($c,$s),array_diff($s,$c));
+                            if(count($change)>0){
+                                $this->alter[$table][$co]=$c;
+                            }
                         }
                     }
                 }
@@ -117,67 +164,88 @@ class mSync{
     function create_table(){
         $schema=  $this->schema;
         foreach($schema as $table=>$cols){
-            $keys="";
-            $sql="CREATE TABLE IF NOT EXISTS `$table` (";
-            foreach ($cols as $c){
-                if(isset($c['Key']) && $c['Key']!=""){
-                    if($c['Key']=="PRI"){
-                        $keys.=" PRIMARY KEY  (`".$c['Field']."`) ";   
-                    }else if($c['Key']=="UNI"){
-                        if($keys!=""){
-                            $keys.=", ";
-                        }
-                        $keys.=" UNIQUE (`".$c['Field']."`) "; 
-                    }else if($c['Key']=="MUL"){
-                        if($keys!=""){
-                            $keys.=", ";
-                        }
-                        $keys.=" INDEX (`".$c['Field']."`) ";  
-                    }
-
-                }
-                $null=" NOT NULL ";
-                if(isset($c['Null'])){
-                    if($c['Null']=="YES"){
-                        $null=" NULL ";
-                    }else if($c['Null']=="NO"){
-                        $null=" NOT NULL ";
-                    }
-                }
-                $default="";
-                if(isset($c['Default']) && $c['Default']!=NULL){
-                    if($c['Default']=='CURRENT_TIMESTAMP'){
-                      $default=" default ".$c['Default']." ";
-
-                    }else{
-                        $default=" default '".$c['Default']."' ";
-                    }
-                    
-                }
-                $extra="";
-                 if(isset($c['Extra'])){
-                     $extra=$c['Extra'];
-                 }
-                $sql.="`".$c['Field']."` ".$c['Type'].$null.$default.$extra.", ";
-            }
-            $sql.=$keys;
-            $sql=rtrim($sql,", ")." );";
-			
-			$result=$this->conn->query("SHOW TABLES LIKE  \"$table\"");
-			
-			$row=$result->fetch_array(MYSQLI_NUM);
-			if(!isset($row[0]))
+            //if view
+            if(isset($cols[0]['View']))
             {
-				if(!$this->conn->query($sql)){
-					echo  mysqli_error($this->conn);
-					echo "\n".$sql;
-					exit ;
-				}
-				else{
-					echo "Table Created : ".$table."\n";
-					error_log(PHP_EOL.$sql, 3, "mSync.log");
-				}
-			}
+                $result=$this->conn->query("SHOW TABLES LIKE  \"$table\"");
+                $row=$result->fetch_array(MYSQLI_NUM);
+                if(!isset($row[0]))
+                {
+                    if(!$this->conn->query($cols[0]['Create View'])){
+                        echo  mysqli_error($this->conn);
+                        echo "\n".$sql;
+                        exit ;
+                    }
+                    else{
+                        echo "View Created : ".$table."\n";
+                        error_log(PHP_EOL.$cols[0]['Create View'], 3, "mSync.log");
+                    }
+                }
+            }
+            else
+            {
+                $keys="";
+                $sql="CREATE TABLE IF NOT EXISTS `$table` (";
+                foreach ($cols as $c){
+                    if(isset($c['Key']) && $c['Key']!=""){
+                        if($c['Key']=="PRI"){
+                            $keys.=" PRIMARY KEY  (`".$c['Field']."`) ";   
+                        }else if($c['Key']=="UNI"){
+                            if($keys!=""){
+                                $keys.=", ";
+                            }
+                            $keys.=" UNIQUE (`".$c['Field']."`) "; 
+                        }else if($c['Key']=="MUL"){
+                            if($keys!=""){
+                                $keys.=", ";
+                            }
+                            $keys.=" INDEX (`".$c['Field']."`) ";  
+                        }
+
+                    }
+                    $null=" NOT NULL ";
+                    if(isset($c['Null'])){
+                        if($c['Null']=="YES"){
+                            $null=" NULL ";
+                        }else if($c['Null']=="NO"){
+                            $null=" NOT NULL ";
+                        }
+                    }
+                    $default="";
+                    if(isset($c['Default']) && $c['Default']!=NULL){
+                        if($c['Default']=='CURRENT_TIMESTAMP'){
+                          $default=" default ".$c['Default']." ";
+
+                        }else{
+                            $default=" default '".$c['Default']."' ";
+                        }
+                        
+                    }
+                    $extra="";
+                     if(isset($c['Extra'])){
+                         $extra=$c['Extra'];
+                     }
+                    $sql.="`".$c['Field']."` ".$c['Type'].$null.$default.$extra.", ";
+                }
+                $sql.=$keys;
+                $sql=rtrim($sql,", ")." );";
+    			
+    			$result=$this->conn->query("SHOW TABLES LIKE  \"$table\"");
+    			
+    			$row=$result->fetch_array(MYSQLI_NUM);
+    			if(!isset($row[0]))
+                {
+    				if(!$this->conn->query($sql)){
+    					echo  mysqli_error($this->conn);
+    					echo "\n".$sql;
+    					exit ;
+    				}
+    				else{
+    					echo "Table Created : ".$table."\n";
+    					error_log(PHP_EOL.$sql, 3, "mSync.log");
+    				}
+    			}
+            }
         }
 		$result=$this->conn->query("SHOW FULL TABLES FROM ".$this->db."  WHERE Table_type != 'VIEW'");
 		while ($row = mysqli_fetch_assoc($result)) {
